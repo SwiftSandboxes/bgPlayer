89ffb3e338feb9606d1f183255c3677837c1d328
Kevin Swift <generalemail7@yahoo.com>
2024-06-18 14:14:41 -0500

    IDE had chosen wrong import for Id

--- src/main/java/swift/sandbox/bg/player/model/BgPlayer.java
+++ src/main/java/swift/sandbox/bg/player/model/BgPlayer.java
@@ -1,27 +1,28 @@
 package swift.sandbox.bg.player.model;
 
-import org.springframework.data.annotation.Id;
-
-import lombok.AllArgsConstructor;
+import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 
 @Entity
-@AllArgsConstructor
 @NoArgsConstructor
+@RequiredArgsConstructor
 public class BgPlayer {
 
     @Id
     @GeneratedValue(strategy=GenerationType.AUTO)
     @Getter
+    @Setter
     private Integer id;
 
     @Getter
+    @Setter
     @NonNull
     private String givenName;
 
